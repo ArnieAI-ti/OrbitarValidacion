97704963b6a9eaebfe17247892a83fdb0db7f120
Arnie AI <arniepaul489@gmail.com>
2026-01-26 12:36:28 -0500

    Updates

--- assets/arrow-left-DScOBb5k.js
+++ assets/arrow-left-DScOBb5k.js
@@ -1,4 +1,4 @@
-import{r as o}from"./index-CUJCNBBQ.js";/**
+import{r as o}from"./index-BJuGkA_I.js";/**
  * @license lucide-react v0.294.0 - ISC
  *
  * This source code is licensed under the ISC license.
