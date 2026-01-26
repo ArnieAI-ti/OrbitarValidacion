53a58d18a9df95400b2301b1529e2b16e5786174
Arnie AI <arniepaul489@gmail.com>
2026-01-26 11:56:29 -0500

    Mover proyecto a nuevo repositorio

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: mode === 'production' ? '/Orbitar---Web-de-validaci-n/' : '/',
+    base: mode === 'production' ? '/OrbitarValidacion/' : '/',
     publicDir: 'public',
 }))
